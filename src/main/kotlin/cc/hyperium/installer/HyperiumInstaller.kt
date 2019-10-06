28db87eb5296bf6d26349a10b2b99e0bcb0417ca
Cubxity <vrcproxz@gmail.com>
2019-10-06 18:13:47 +0200

    update copyright

--- src/main/kotlin/cc/hyperium/installer/HyperiumInstaller.kt
+++ src/main/kotlin/cc/hyperium/installer/HyperiumInstaller.kt
@@ -1,5 +1,12 @@
 /*
- * Copyright (C) 2019 Cubxity. All Rights Reserved.
+ * Copyright © 2019 by Sk1er LLC
+ *
+ * All rights reserved.
+ *
+ * Sk1er LLC
+ * 444 S Fulton Ave
+ * Mount Vernon, NY
+ * sk1er.club
  */
 
 package cc.hyperium.installer
@@ -8,6 +15,7 @@ import cc.hyperium.installer.ui.InstallerStyles
 import cc.hyperium.installer.ui.InstallerView
 import tornadofx.App
 import tornadofx.launch
+import java.io.File
 import javax.swing.JOptionPane
 import javax.swing.UIManager
 
@@ -28,5 +36,14 @@ fun main() {
             "Class not found",
             JOptionPane.WARNING_MESSAGE
         )
+    }
+}
+
+private fun getBundledJFX(): File? {
+    val os = System.getProperty("os.name").toLowerCase()
+    return when {
+        os.contains("mac") -> File(System.getProperty("user.dir"), "Library/Application Support/minecraft/runtime/jre-x64/jre.bundle/Contents/Home/lib/ext/jfxrt.jar")
+        os.contains("win") -> File("C:\\Program Files (x86)\\Minecraft Launcher\\runtime\\jre-x64\\lib\\ext\\jfxrt.jar")
+        else -> null
     }
 }
--- src/main/kotlin/cc/hyperium/installer/shared/entities/addon/AddonsManifest.kt
+++ src/main/kotlin/cc/hyperium/installer/shared/entities/addon/AddonsManifest.kt
@@ -1,5 +1,12 @@
 /*
- * Copyright (C) 2019 Cubxity. All Rights Reserved.
+ * Copyright © 2019 by Sk1er LLC
+ *
+ * All rights reserved.
+ *
+ * Sk1er LLC
+ * 444 S Fulton Ave
+ * Mount Vernon, NY
+ * sk1er.club
  */
 
 package cc.hyperium.installer.shared.entities.addon
